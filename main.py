2c863889c180f79dc938df6e5c403172ee5cf095
Mazino <fredozv01@gmail.com>
2024-12-29 23:13:22 +0100

    def escalier

--- main.py
+++ main.py
@@ -2,22 +2,16 @@ import turtle
 
 # Créer une fenêtre et une tortue
 t = turtle.Turtle()
-
-
-
-for i in range(5) :   
-    t.forward(50) #avance 
-    t.left(90) #tourne gauche en degre 
-    t.forward(50) #avance 
-    t.right(90)     
-t.forward(50)     
+def escalier(taille,nb):
+    for i in range(0,nb) :   
+        t.forward(taille) #avance 
+        t.left(90) #tourne gauche en degre 
+        t.forward(taille) #avance 
+        t.right(90) #tourne droit en degre
+        # taille=taille-10
+        taille-=10               
+    t.forward(taille)     
     
-
+escalier(60,5)
 # Garder la fenêtre ouverte
 turtle.done()
-
-
-#t.forward(100)
-
-#t.backward(100) #arriere 
-#t.right(47) #droit
