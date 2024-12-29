8b03fe483e85ddcd88236da1eeaca3d110e309eb
Mazino <fredozv01@gmail.com>
2024-12-29 23:03:11 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import turtle
+
+# Créer une fenêtre et une tortue
+t = turtle.Turtle()
+
+
+
+for i in range(5) :   
+    t.forward(50) #avance 
+    t.left(90) #tourne gauche en degre 
+    t.forward(50) #avance 
+    t.right(90)     
+t.forward(50)     
+    
+
+# Garder la fenêtre ouverte
+turtle.done()
+
+
+#t.forward(100)
+
+#t.backward(100) #arriere 
+#t.right(47) #droit
